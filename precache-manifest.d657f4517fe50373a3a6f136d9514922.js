3e2977d52250cad4a7de1ba38482922c88772612
christiaan <cjlamprecht+github@googlemail.com>
2021-03-18 22:20:00 +0000

    Updates

---
+++ precache-manifest.d657f4517fe50373a3a6f136d9514922.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f30bb070b7da8081d0ecb79f872a2170",
+    "url": "/react-redux-template/index.html"
+  },
+  {
+    "revision": "fc96c5478db214a2cca5",
+    "url": "/react-redux-template/static/js/2.b1e0e125.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/react-redux-template/static/js/2.b1e0e125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4c5f6115d7044593b90",
+    "url": "/react-redux-template/static/js/main.6c33cb0a.chunk.js"
+  },
+  {
+    "revision": "2e19d1aea68a028e0a96",
+    "url": "/react-redux-template/static/js/runtime-main.8fc1046f.js"
+  }
+]);
